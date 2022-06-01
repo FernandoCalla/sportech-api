4a552b52f76c5baf54e4c5d0fdbddd4bb458e53d
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-05-31 23:45:07 -0500

    add mongo atlas connection

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose'
 
 export default function connectDb() {
-  const url = "mongodb://localhost:27017/sportech-api"
+  const url = `mongodb+srv://SporTech:${process.env.MONGO_ATLAS}@cluster0.qvpevds.mongodb.net/SporTech?retryWrites=true&w=majority`
 
   mongoose.connect(url, {
     useNewUrlParser: true
   })
 
   mongoose.connection.on('error', (error) => {
-    log.error(error)
+    console.error(error)
   })
 }
